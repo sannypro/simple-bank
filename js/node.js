fbd09aa97ef8cec03b80116301c4642048874910
sannypro <sannymr79@gmail.com>
2022-02-10 21:29:26 +0600

    balance change

--- js/node.js
+++ js/node.js
@@ -3,6 +3,6 @@ loginButton.addEventListener('click', function () {
     const userEmail = document.getElementById('email').value;
     const userPass = document.getElementById('pass').value;
     if (userEmail == 'sadiasultana0303@gmail.com' && userPass == "sannysadia") {
-        window.location.href = "../bank.html";
+        window.location.href = "https://sannypro.github.io/simple-bank/bank.html";
     }
 })
